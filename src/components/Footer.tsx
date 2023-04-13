bed01b79f246854d3e06df6755ba0b32bb6176c7
mejiabrayan <learnwithbrayan@gmail.com>
2023-04-13 09:41:32 -0700

    updated footer to flow with the rest of the page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import { buttonVariants } from '@/ui/Button';
 
 const Footer = () => {
   return (
-    <div className='bg-white/75 dark:bg-slate-900/75 border-t border-slate-300 dark:border-slate-700 fixed bottom-0 w-full'>
-      <div className='container max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center'>
+    <div className='bg-white/75 dark:bg-slate-900/75 border-t border-slate-300 dark:border-slate-700 w-full'>
+      <div className='container max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center mt-auto'>
         <div className='text-sm text-gray-500 dark:text-gray-400'>
           &copy; {new Date().getFullYear()} Creavio. All rights reserved.
         </div>
